f1e9f864adfab138e94853e5ff7919edb0afa281
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-03-23 21:45:56 +0530

    add user model with schema definition for name, email, and password

---
+++ backend/models/user.models.js
@@ -0,0 +1,20 @@
+import mongoose,{Schema} from "mongoose";
+const userSchema = new Schema({
+    name: {
+        type : String,
+        required : true
+    },
+    email: {
+        type : String,
+        required : true,
+        unique : true,
+        toLowercase : true
+    },
+    password: {
+        type : String,
+        required : true
+    }
+
+})
+
+export const User = mongoose.model("User", userSchema)
