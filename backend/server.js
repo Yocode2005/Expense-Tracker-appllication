f9b2b21fc63765d5ca31c439b789b3bd5d199149
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-03-23 19:13:25 +0530

    update server.js to include dotenv configuration and enhance middleware setup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,21 @@
 import express from 'express'
 import cors from 'cors'
+import { configDotenv } from 'dotenv'
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// DB connection
+
+
+// routes
+app.get('/',(req,res) => {
+        res.send('Hello World!');
+})
+
+app.listen(PORT,() => {console.log(`Server is running on port ${PORT}`)})
