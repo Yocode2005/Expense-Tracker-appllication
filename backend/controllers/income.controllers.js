dc09fd18b4574a694e1960c7e899df06b01c1c91
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-03-28 13:51:48 +0530

    feat: add controller function to fetch all income entries for a user

--- backend/controllers/income.controllers.js
+++ backend/controllers/income.controllers.js
@@ -3,6 +3,7 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import {ApiError} from "../utils/ApiError.js";
 
+// Controller function to add income
 const addIncome = asyncHandler(async(req, res) => {
     // todolist: validate data coming from frontend
     // todolist: create income entry in db
@@ -32,5 +33,19 @@ try {
 } catch (error) {
     throw new ApiError(500, "Error while adding income")
 }
+});
+
+// Controller function to get all income entries of a user
+const getAllIncomes = asyncHandler(async(req, res) => {
+    // todolist: get user id from req.user
+    // todolist: fetch all income entries of the user from db
+    // todolist: send response to frontend
+    const userId = req.user._id;
+    try {
+        const incomes = await Income.find({userId}).sort({createdAt : -1});
+        res.status(200).json(new ApiResponse(true, "Incomes fetched successfully", incomes))
+    } catch (error) {
+        throw new ApiError(500, "Error while fetching incomes")
+    }
 });
 
