c47b8419993354c171b5bcfbf5e1f3f7883e7bfb
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-04-04 20:50:43 +0530

    feat: add state management and navigation to Login component

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { loginStyles } from '../assets/dummyStyles'
 import { User } from 'lucide-react'
 
 
 const Login = ({onLogin, API_URL = "http://localhost:5000/api"}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [remberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
   return (
     <div className={loginStyles.pageContainer}>
       <div className={loginStyles.cardContainer}>
