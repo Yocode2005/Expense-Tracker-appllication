00fdc4ff45caf1e9c4df035c3c0ad0146fdda9d8
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-04-04 20:45:00 +0530

    feat: update Login component to include welcome message and subtitle

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { loginStyles } from '../assets/dummyStyles'
+import { User } from 'lucide-react'
 
 
 const Login = ({onLogin, API_URL = "http://localhost:5000/api"}) => {
@@ -10,7 +11,8 @@ const Login = ({onLogin, API_URL = "http://localhost:5000/api"}) => {
           <div className={loginStyles.avatar}>
             <User className=" w-10 h-10 text-white" />
           </div>
-          <h1></h1>
+          <h1 className={loginStyles.headerTitle}>Welcome Back</h1>
+          <p className={loginStyles.headerSubtitle}>Sign in to your ExpenseTracker account</p>
         </div>
       </div>
     </div>
