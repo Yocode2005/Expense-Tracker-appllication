e9d10d466c25c0df6c4d9a6eaa40847ba0122091
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-04-15 00:00:22 +0530

    fix the variable name

--- frontend/src/components/FinancialCard.jsx
+++ frontend/src/components/FinancialCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FinancialCard = ({icons, label, value, additionalContent, borderColor = "", bgColor="bg-white"}) => (
     <div className={`${bgColor} rounded-xl p-5 lg:-mx-2 lg:p-2 shadow-sm border hover:shadow-md border-gray-100 transition-all ${borderColor}`}>
         <div className="text-sm font-medium text-gray-600 flex items-center gap-2">
-            {icon}
+            {icons}
             {label}
         </div>
         <p className='text-2xl font-bold text-gray-800 mt-1'>
