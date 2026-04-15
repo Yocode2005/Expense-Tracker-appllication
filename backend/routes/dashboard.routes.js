0bb30035a5cf6f7286743960e899e2dd8656ebb0
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-04-15 12:14:34 +0530

    fix the variable name

--- backend/routes/dashboard.routes.js
+++ backend/routes/dashboard.routes.js
@@ -3,6 +3,6 @@ import { verifyJWT } from '../middleware/auth.middleware.js';
 import { getDashboardOverview } from '../controllers/dashboard.controllers.js';
 const dashboardRouter = Router();
 // protected route to get dashboard overview
-dashboardRouter.route("/overview").get(verifyJWT, getDashboardOverview);
+dashboardRouter.route("/").get(verifyJWT, getDashboardOverview);
 
 export default dashboardRouter;
