6fa2720bf07d3abe6285a9d939024c5fd5bbbf86
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-04-16 18:42:33 +0530

    import activity

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import { Activity, useEffect, useMemo, useState } from "react";
+import { Activity } from "lucide-react";
 import { styles } from "../assets/dummyStyles.js";
 import Navbar from "./Navbar.jsx";
 import Sidebar from "./Sidebar.jsx";
